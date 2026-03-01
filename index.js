fed96eaec46309c04751bf0b17dba8f94929a12e
Jadonfdnd <jadon.tjon@hva.nl>
2026-03-01 15:23:09 +0100

    #2  added toggle for animation

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-let trackForm = document.querySelector('#track-form');
-let searchBtn = document.querySelector('#search-btn');
+const trackForm = document.querySelector('#track-form');
+const searchBtn = document.querySelector('#search-btn');
 
 trackForm.addEventListener('submit', (e) => {
   e.preventDefault(); 
 
   // Feedback
-  searchBtn.classList.add('is-loading');
+  searchBtn.classList.toggle('is-loading');
 
   // 
   setTimeout(() => {
     console.log("Zoek voltooid");
-  }, 1200);
+  }, 100);
+});
+
+
+
+
+// click and scale animatie
+const flowButton = document.querySelector('.mini-cards a');
+
+flowButton.addEventListener("mousedown", function () {
+  flowButton.classList.add("flowCss");
+});
+
+
+flowButton.addEventListener("mouseup", function () {
+  flowButton.classList.remove("flowCss");
 });
